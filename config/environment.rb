fbc5f09a6b97b761b2b2d8628cac6600bba43d8d
Samip Sharma <49893100+samip-sharma@users.noreply.github.com>
2019-06-17 14:42:28 -0400

    Add files via upload

---
+++ config/environment.rb
@@ -0,0 +1,19 @@
+
+require 'rake'
+require 'active_record'
+require 'yaml/store'
+require 'ostruct'
+require 'date'
+
+require 'bundler/setup'
+Bundler.require
+
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: "db/playlist.sqlite"
+)
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
