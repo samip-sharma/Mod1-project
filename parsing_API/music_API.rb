cdea0db3d3c48d3af50be620f7f196e2827b7b34
github username <github email address>
2019-06-18 10:50:21 -0400

    add music_Api

--- parsing_API/music_API.rb
+++ parsing_API/music_API.rb
@@ -1,6 +1,18 @@
 require "rest-client"
+require "pry"
+require "json"
 
 def musicAPI(artist, song)
-  url =
-    character_data = RestClient.get("http://swapi.co/api/people/1")
+  artist = artist.split(" ").join("_")
+  song = song.split(" ").join("_")
+  url = "https://api.deezer.com/search?q=#{artist}_#{song}"
+  music_data = RestClient.get(url)
+  hash = JSON.parse(music_data)
+  song_title = hash["data"][0]["title"]
+  song_preview = hash["data"][0]["preview"]
+  artist_name = hash["data"][0]["artist"]["name"]
+  cover_picture = hash["data"][0]["artist"]["picture_big"]
+  binding.pry
 end
+
+print musicAPI("John Maus", "Rockets")
