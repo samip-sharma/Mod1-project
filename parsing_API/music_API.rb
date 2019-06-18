33528ae6ea7e53a5be1a2fed8990735f116f5e92
github username <github email address>
2019-06-18 12:51:41 -0400

    Testing music_API play back

--- parsing_API/music_API.rb
+++ parsing_API/music_API.rb
@@ -2,17 +2,26 @@ require "rest-client"
 require "pry"
 require "json"
 
+## To Do: Refactor this code to be encapsulated in a class
+
+$song_title = ""
+$song_preview = ""
+$artist_name = ""
+$cover_picture = ""
+
 def musicAPI(artist, song)
   artist = artist.split(" ").join("_")
   song = song.split(" ").join("_")
   url = "https://api.deezer.com/search?q=#{artist}_#{song}"
   music_data = RestClient.get(url)
   hash = JSON.parse(music_data)
-  song_title = hash["data"][0]["title"]
-  song_preview = hash["data"][0]["preview"]
-  artist_name = hash["data"][0]["artist"]["name"]
-  cover_picture = hash["data"][0]["artist"]["picture_big"]
-  binding.pry
+  $song_title = hash["data"][0]["title"]
+  $song_preview = hash["data"][0]["preview"]
+  $artist_name = hash["data"][0]["artist"]["name"]
+  $cover_picture = hash["data"][0]["artist"]["picture_big"]
+  # binding.pry
 end
 
-print musicAPI("John Maus", "Rockets")
+def music_Preview
+  $song_preview
+end
---
+++ parsing_API/play_music.rb
@@ -0,0 +1,5 @@
+require_relative "./music_API.rb"
+
+musicAPI("John Maus", "Rockets")
+
+puts music_Preview
